1674d585cb092a658325c088fd72c18939386971
Juddy2403 <119872018+Juddy2403@users.noreply.github.com>
2025-06-29 23:55:03 +0200

    updated player modifiers -> interface

--- Source/EchoOfThePast/Private/PlayerStatsInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PlayerStatsInterface.h"
-
-// Add default functionality here for any IPlayerStatsInterface functions that are not pure virtual.
